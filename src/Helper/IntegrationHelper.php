2230760c91c0602809ad1ea55d7953104f6cc87f
Septdir <iberdichevskiy@septdir.com>
2026-02-26 16:29:55 +0300

    Prepare 2.0.1 Release

--- src/Helper/IntegrationHelper.php
+++ src/Helper/IntegrationHelper.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Payment Tinkoff Plugin
  * @subpackage  plg_radicalmart_payment_tinkoff
- * @version     __DEPLOY_VERSION__
+ * @version     2.0.1
  * @author      RadicalMart Team - radicalmart.ru
  * @copyright   Copyright (c) 2026 RadicalMart. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
